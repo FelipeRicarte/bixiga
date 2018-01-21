c3bb7d6aed8a51209a4f382fafb6f48eda4b0832
Felipe Ricarte <felipericartem@gmail.com>
2018-01-21 19:43:37 -0300

    estrutura inicial

---
+++ app/Cliente.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cliente extends Model
+{
+    public function telefones(){
+        return $this->hasMany('App\Telefone');
+    }
+
+    public function addTelefone(Telefone $telefone){
+        return this->telefones()->save($telefone);
+    }
+}
